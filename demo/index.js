a5c2a36ee00c8fc51735f6ed3e749ff44a465109
Carlos Silva Villamizar <carlos.silva@highdeveloper.com>
2015-06-12 19:10:56 -0500

    New simple base and structure

---
+++ demo/index.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+
+    var ajaxui = AjaxUI();
+    ajaxui.startService();
+});
---
+++ server.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var    fs = require('fs');
+
+fs.readFile('./demo/index.html', function (err, html) {
+    if (err) {
+        throw err;
+    }
+    http.createServer(function(request, response) {
+        response.writeHeader(200, {"Content-Type": "text/html"});
+        response.write(html);
+        response.end();
+    }).listen(3000);
+});
