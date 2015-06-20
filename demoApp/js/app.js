03be9e490ddb123ac4578669384b5fa6be262f6c
Carlos Silva Villamizar <carlos.silva@highdeveloper.com>
2015-06-19 19:24:42 -0500

    Simple example config AjaxuiJS in app.js on demoApp folder

--- demoApp/js/app.js
+++ demoApp/js/app.js
@@ -2,10 +2,33 @@ $(document).ready(function(){
 
     var ajaxui = new Ajaxui();
     ajaxui.settings({
-        loaderClass: 'cube-transition'
+        loaderClass: 'ball-scale-multiple'
+    });
+    ajaxui.callbacks({
+        ajax: {
+            enabled: true,
+            success: function(data){
+                alert(data);
+            }
+        },
+        actions: {
+            enabled: true,
+            functions: {
+                'nameFunc1': function(){
+                    console.log("sample1");
+                },
+                'nameFunc2': function(){
+                    console.log('sample2')
+                },
+                'nameFunc3': numFunc3
+            }
+        }
     });
     ajaxui.startService();
 
 });
 
 
+function numFunc3(){
+    alert("sample3");
+}
